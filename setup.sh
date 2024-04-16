25277e5e04209e810c7665067f59e2e3fe9eef14
Rvaldebon <rvaldebon@gmail.com>
2024-04-16 12:24:19 +0200

    Update script bash

--- setup.sh
+++ setup.sh
@@ -8,14 +8,14 @@ wait_for_seconds() {
 }
 
 # Répertoires où exécuter "vagrant up"
-directories=("chemin/vers/repertoire1" "chemin/vers/repertoire2" "chemin/vers/repertoire3")
+directories=("./Linux" "../Windows_Client" "../Windows_Server")
 
 # Exécuter "vagrant up" dans chaque répertoire et attendre 10 secondes entre chaque
 for dir in "${directories[@]}"; do
   echo "Se déplaçant vers le répertoire : $dir"
   cd "$dir" || exit 1
   vagrant up
-  wait_for_seconds 10
+  wait_for_seconds 5
 done
 
 # Lancer VirtualBox
